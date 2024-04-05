b98045f221957f1c8e423056d3d9f715d56ecf5f
vishalx06 <vishalx06@gmail.com>
2024-04-05 22:07:35 +0530

    Initial Commit

---
+++ swiftBootCampPart2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftBootCampPart2
+//
+//  Created by Vishal Sharma on 05/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftBootCampPart2/swiftBootCampPart2App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftBootCampPart2App.swift
+//  swiftBootCampPart2
+//
+//  Created by Vishal Sharma on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftBootCampPart2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
